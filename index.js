5bcdb09a6800cc4a9d5e1db65ba21b8e3b2453d7
MotionStudio <62178936+MotionStudioDev@users.noreply.github.com>
2026-03-16 23:20:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 // Express server (7/24 aktif kalmak için)
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 app.get('/', (req, res) => {
   res.sendStatus(200);
